61d2ab0da6a7758a493b5cf6654aa5fc8a2248aa
tusharbhoge <tusharbhoge80@gmil.com>
2024-10-06 18:40:52 +0530

    final

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const userModel = require("./model/user")
+const app=express();
+
+app.set("view engine", "ejs")
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.render('index');
+})
+
+app.get("/read",async(req,res)=>{
+    let users = await userModel.find();
+    res.render('read', {users});
+})
+
+app.get("/delete/:id",async(req,res)=>{
+    let users = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect('/read');
+})
+
+app.post("/create",async(req,res)=>{
+    let {name, email, image} = req.body;
+   let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    });
+
+    res.redirect("/read");
+})
+
+
+app.listen(3000);
